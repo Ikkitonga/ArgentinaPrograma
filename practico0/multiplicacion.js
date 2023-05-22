109e56af5f20c2168881f0e9c5671dadf7113f9c
ikkitonga <mcgaston@gmail.com>
2023-05-22 13:32:22 -0300

    el resultado de la multiplicacion es 204

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+function multiplicacion(num1, num2, num3){
+    var num1= 2;
+    var num2= 17;
+    var num3= 6;
+        multiplicacion= (num1 * num2 * num3);
+            return multiplicacion
+}
