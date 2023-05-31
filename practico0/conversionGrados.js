cd647ca2468e30390be7f53a98814a9879cbef78
ikkitonga <mcgaston@gmail.com>
2023-05-31 12:02:58 -0300

    “Agrego el archivo conversionGrados.js”.

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,8 @@
+function conversionGrados(farenheit){
+    var farenheit = 50;
+        conversionGrados = (farenheit - 32) * 5/9.0;
+    return conversionGrados
+} 
+
+/*50°F son 10°C 
+75°F son 23.8889°C*/
