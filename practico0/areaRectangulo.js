7df10cff034a536a220bbde406b27b69a8040cb6
ikkitonga <mcgaston@gmail.com>
2023-05-31 12:12:09 -0300

    “Practico N0 finalizado”

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,9 @@
+function areaRectangulo(base, altura){
+    var base= 5;
+    var altura= 3;
+        areaRectangulo= (base * altura);
+        return areaRectangulo
+}
+
+/* en el ejercicio "a" el area es 15*/
+/* en el ejercicio "b" el area es 66.25*/
---
+++ practico0/promedio3.js
@@ -0,0 +1,11 @@
+function promedio3(num1, num2, num3){
+    var num1= 5;
+    var num2= 7;
+    var num3= 9;
+        promedio3 = (num1+ num2+ num3)/3;
+        return promedio3
+}
+
+/*el resultado para el ejercicio "a" es 7*/
+/*el resultado para el ejercicio "b" es -4.33*/
+/*el resultado para el ejercicio "c" es 0.2666*/
