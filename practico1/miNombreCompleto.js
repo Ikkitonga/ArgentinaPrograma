dc3d95467930095e7cf9a92df63ada10fc6a95b1
ikkitonga <mcgaston@gmail.com>
2023-06-08 17:16:53 -0300

    primer ejercicio variables

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,10 @@
+let nombre;
+nombre = "Gaston";
+let apellido = "Macias";
+let edad = 38;
+let miNombreCompleto = "Hola, " + nombre + " " + apellido + "." + " Tienes " + edad + " años.";
+
+console.log(miNombreCompleto)
+
+
+
